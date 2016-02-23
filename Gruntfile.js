f6d3227a3286f16956898f9103276d1caa794537
Ivo Roberto Batistela <byivo@byivo-personal>
2016-02-23 01:21:39 -0300

    corrigido falha de busca ao arquivo de tradução do fullcalendar

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports= function(grunt) {
         '**/jquery.min.js',
         '**/moment.min.js',
         '**/fullcalendar.min.js',
-        '**/pt-br.min.js',
+        '**/pt-br.js',
         '**/underscore-min.js'
     ],
 
